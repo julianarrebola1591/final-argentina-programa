7e507f30f093692c495795a81a4e4b7429d6fca7
Pablo98L <lopezpablo.m98@gmail.com>
2023-04-18 19:19:44 -0300

    add un script de Tooltip de Bootstrap

--- script/main.js
+++ script/main.js
@@ -128,3 +128,6 @@ $("#btnEnviarContacto").click(function()
     console.log(mensaje);
     
 });
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
